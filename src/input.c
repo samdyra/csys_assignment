83775b48783e076b5f759bccb0bd90b9f66dff59
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2026-05-19 22:42:47 +1000

    chore: improve comment

--- src/input.c
+++ src/input.c
@@ -84,7 +84,7 @@ void listen_joystick_input(void) {
         interval_ms = 50;  // fast
     }
 
-    if (shared_counter_0 - last_scroll_tick < interval_ms) return;
+    if (shared_counter_0 - last_scroll_tick < interval_ms) return;  // rate limiter
     last_scroll_tick = shared_counter_0;
 
     if (direction > 0) {
